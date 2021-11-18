f2d9dddc243f6b62fe600e8cb054ba0af9eccd5e
guruputran <gpsmartcodes@gmail.com>
2021-11-18 12:24:16 +0800

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+/** @format */
+
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer className="py-5 bg-black">
+        <div className="container px-5">
+          <p className="m-0 text-center text-white small">
+            Copyright &copy; Your Website 2021
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
